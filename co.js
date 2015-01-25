3efc14bdc3acef604e548db61d100bc27bfe7ce9
let4be <let4be@gmail.com>
2015-01-25 13:07:00 +0300

    Let's rock!

---
+++ co.js
@@ -0,0 +1,106 @@
+angular.module('angular-co', []).factory('co', function($q, $rootScope, $exceptionHandler) {
+	var createGeneratorProxy = (gen) => function *() {
+		try {
+			return yield gen;
+		} catch (err) {
+			$exceptionHandler(err);
+
+			throw err;
+		} finally {
+			$rootScope.$apply();
+		}
+	};
+
+	var co = function (gen) {
+		var deferred = $q.defer();
+
+		coJS(createGeneratorProxy(gen))
+			.then(function () {
+				deferred.resolve.apply(deferred.resolve, arguments);
+			})
+			.catch(function (err){
+				deferred.reject(err);
+			});
+
+		return deferred.promise;
+	};
+
+	co.transform = function (gen, transform) {
+		return co(function *(){
+			return transform(yield gen);
+		});
+	};
+
+	co.each = function (object, mapGen) {
+		return co(function *() {
+			if (angular.isArray(object)) {
+				for (var i = 0; i < object.length; i++) {
+					yield mapGen(object[i], i, object);
+				}
+			} else if (angular.isObject(object)) {
+				for (var k of Object.keys(object)) {
+					yield mapGen(object[k], k, object);
+				}
+			}
+		});
+	};
+
+	co.map = function (object, mapGen, init) {
+		if (!object)
+			return init;
+
+		return co(function *() {
+			var r;
+			if (angular.isArray(object)) {
+				r = [];
+				for (var i = 0; i < object.length; i++) {
+					r[i] = yield mapGen(object[i], i, object);
+				}
+			} else if (angular.isObject(object)) {
+				r = {};
+				for (var k of Object.keys(object)) {
+					r[k] = yield mapGen(object[k], k, object);
+				}
+			}
+			return r;
+		});
+	};
+
+	co.reduce = function (object, reduceGen, init) {
+		if (!object)
+			return init;
+
+		return co(function *() {
+			if (angular.isArray(object)) {
+				for (var i = 0; i < object.length; i++) {
+					init = yield reduceGen(init, object[i], i, object);
+				}
+			} else if (angular.isObject(object)) {
+				for (var k of Object.keys(object)) {
+					init = yield reduceGen(init, object[k], k, object);
+				}
+			}
+			return init;
+		});
+	};
+
+	co.wrap = function (gen) {
+		var coFn = coJS.wrap(createGeneratorProxy(gen));
+
+		return function () {
+			var deferred = $q.defer();
+			coFn
+				.apply(coFn, arguments)
+				.then(function () {
+					deferred.resolve.apply(deferred.resolve, arguments);
+				})
+				.catch(function (err){
+					deferred.reject(err);
+				});
+			return deferred.promise;
+		};
+	};
+
+	return co;
+});
+
